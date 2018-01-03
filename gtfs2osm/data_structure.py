47ee8359821947b36f3f97d2c7df6cd2bda6263f
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-01-03 11:32:51 +0100

    Change data structure to handle the latest added fields

---
+++ gtfs2osm/data_structure.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, ForeignKey, ForeignKeyConstraint, UniqueConstraint
+from sqlalchemy import Integer, Unicode
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import synonym, relationship
+
+Base = declarative_base()
+
+class POI_address(Base):
+    __tablename__ = 'poi_address'
+    _plural_name_ = 'poi_address'
+    pa_id = Column(Integer, primary_key=True, index=True)
+    id = synonym('pa_id')
+    #poi_osm_id = Column(Integer, nullable=False, unique=True, index=True)
+    poi_osm_id = Column(Integer, index=True)
+    poi_name = Column(Unicode, nullable=False, index=True)
+    poi_branch = Column(Unicode, nullable=True, index=True)
+    poi_shop = Column(Unicode, nullable=False, index=True)
+    poi_addr_city = Column(Integer, ForeignKey('city.city_id'), index=True)
+    poi_postcode = Column(Integer)
+    poi_city = Column(Unicode)
+    poi_addr_street = Column(Unicode)
+    poi_addr_housenumber = Column(Unicode)
+    poi_conscriptionnumber = Column(Unicode)
+    original = Column(Unicode)
+    poi_website = Column(Unicode)
+
+    def __repr__(self):
+        return '<POI address {}: {}>'.format(self.poi_id, self.poi_name)
+
+class City(Base):
+    __tablename__ = 'city'
+    _plural_name_ = 'city'
+    city_id = Column(Integer, primary_key=True, index=True)
+    id = synonym('city_id')
+    city_name = Column(Unicode)
+    city_post_code = Column(Integer)
+
+    __table_args__ = (UniqueConstraint('city_name', 'city_post_code', name='uc_city_name_post_code'), )
+
+    def __repr__(self):
+        return '<City {}: {} ({})>'.format(self.city_id, self.city_name, self.city_post_code)
